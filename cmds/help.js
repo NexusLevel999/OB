96feaeff457052f6ba5e126a826e88eedbafcc8c
NexusLevel999 <nexuskaizu@gmail.com>
2024-05-22 06:49:48 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -5,15 +5,12 @@ module.exports = {
     cooldown: 16,		
     execute(api, event, args, commands) {
         let helpMessage = 'Available Cmds\n';
-        helpMessage += '💮═══════════════💮\n';
         commands.forEach((command, name) => {
-            helpMessage += `𝙽𝚊𝚖𝚎: ${name}\n`;
-            helpMessage += `𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚝𝚒𝚘𝚗: ${command.description}\n`;
-            helpMessage += `𝚁𝚘𝚕𝚎: ${command.role}\n`;
+            helpMessage += `Name: ${name}\n`;
+            helpMessage += `Description: ${command.description}\n`;
+            helpMessage += `Role: ${command.role}\n`;
             helpMessage += `Credits: ${command.credits}\n`;
-        helpMessage += '💮═══════════════💮\n';
         });
-        helpMessage += 'https://facebook.com/kaizu.ui';
         api.sendMessage(helpMessage, event.threadID);
     }
 };

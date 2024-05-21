47bac20478d9d3d45c4e6e62240b7caeef7d8a48
NexusLevel999 <nexuskaizu@gmail.com>
2024-05-22 07:06:26 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,15 +1,16 @@
 module.exports = {
-    description: "Show Commands and the descriptions",
+    description: "Show Commands and their descriptions",
     role: "user",
     credits: "rejard",
-    cooldown: 16,		
+    cooldown: 16,
     execute(api, event, args, commands) {
-        let helpMessage = 'Available Cmds\n';
+        let helpMessage = 'Available Commands:\n\n';
         commands.forEach((command, name) => {
             helpMessage += `Name: ${name}\n`;
             helpMessage += `Description: ${command.description}\n`;
             helpMessage += `Role: ${command.role}\n`;
             helpMessage += `Credits: ${command.credits}\n`;
+            helpMessage += '------------------------\n';
         });
         api.sendMessage(helpMessage, event.threadID);
     }

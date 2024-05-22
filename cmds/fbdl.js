6f0550dc9442dc91bef114af576b4f9ace1e7e26
NexusLevel999 <nexuskaizu@gmail.com>
2024-05-22 08:26:32 +0800

    Create fbdl.js

---
+++ cmds/fbdl.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+module.exports = {
+    description: "Fbdown",
+    role: "user",
+    credits: "nexk",
+    cooldown: 10,
+    execute: async function(api, event, args, commands) {
+        if (args.length === 0) {
+            return api.sendMessage("Please provide a URL.", event.threadID);
+        }
+
+        const url = encodeURIComponent(args.join(" "));
+        const apiUrl = `http://94.130.129.40:8370/facebook?url=${url}`;
+
+        try {
+            const response = await axios.get(apiUrl);
+            if (response.data.error) {
+                api.sendMessage(`Error: ${response.data.error}`, event.threadID);
+            } else if (!response.data.data) {
+                api.sendMessage("No Facebook data found.", event.threadID);
+            } else {
+                api.sendMessage(response.data.data, event.threadID);
+            }
+        } catch (error) {
+            console.error(`Error fetching Facebook data: ${error}`);
+            api.sendMessage("An error occurred while fetching Facebook data.", event.threadID);
+        }
+    }
+};

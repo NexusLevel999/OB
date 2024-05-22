86febb98360891e09da738921dc920099a18aaff
NexusLevel999 <nexuskaizu@gmail.com>
2024-05-22 18:42:05 +0800

    Update uid.js

--- cmds/uid.js
+++ cmds/uid.js
@@ -1,10 +1,11 @@
 module.exports = {
     description: "Show the UID of the user",
+    aliases: ["un","unsent"],
     role: "user",
     cooldown: 15,
     execute(api, event, args, commands) {
         const userID = event.senderID;
       
-        api.sendMessage(`Your User ID (UID) is: ${userID}`, event.threadID, event.messageID);
+        api.sendMessage(`${userID}`, event.threadID, event.messageID);
     }
 };
